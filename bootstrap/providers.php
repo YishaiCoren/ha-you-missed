94ac8770852c5c874f6adeea775581bb1a837160
YishaiCoren <corenyishai@gmail.com>
2026-05-03 18:57:54 +0300

    Register Makerloft reporter provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use App\Providers\MakerloftReporterServiceProvider;
 
 return [
     AppServiceProvider::class,
+    MakerloftReporterServiceProvider::class,
 ];
